bd448d72392f1a32c6233ba1c2d760c413d30127
MahmoudT123 <mahmoud3570sa@gmail.com>
2024-12-08 20:16:21 +0200

    create reset password feature

--- lib/features/Auth/auth_page/presentation/auth_page.dart
+++ lib/features/Auth/auth_page/presentation/auth_page.dart
@@ -1,6 +1,5 @@
 import 'package:courseup/features/Auth/Login/presentation/views/my_login_view.dart';
 import 'package:courseup/features/Auth/sharedPresentation/cubit/auth_cubit.dart';
-import 'package:courseup/features/Home/presentation/views/my_home_view.dart';
 import 'package:courseup/features/ViewProfile/presentation/views/my_view_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
